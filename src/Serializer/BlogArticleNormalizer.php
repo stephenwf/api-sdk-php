7bb677c6801bb6f31a1dfcef5343c70ad76977a5
Stephen Fraser <stephenwf@users.noreply.github.com>
2016-09-27 11:09:58 +0100

    Fixed blog articles (#22)

--- src/Serializer/BlogArticleNormalizer.php
+++ src/Serializer/BlogArticleNormalizer.php
@@ -13,7 +13,6 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use function GuzzleHttp\Promise\all;
 use function GuzzleHttp\Promise\promise_for;
 
 final class BlogArticleNormalizer implements NormalizerInterface, DenormalizerInterface, NormalizerAwareInterface, DenormalizerAwareInterface
@@ -66,16 +65,16 @@ final class BlogArticleNormalizer implements NormalizerInterface, DenormalizerIn
         if ($object->hasSubjects()) {
             $data['subjects'] = $object->getSubjects()->map(function (Subject $subject) {
                 return $subject->getId();
-            });
+            })->toArray();
         }
 
         if (empty($context['snippet'])) {
             $data['content'] = $object->getContent()->map(function (Block $block) use ($format, $context) {
                 return $this->normalizer->normalize($block, $format, $context);
-            });
+            })->toArray();
         }
 
-        return all($data)->wait();
+        return $data;
     }
 
     public function supportsNormalization($data, $format = null) : bool
